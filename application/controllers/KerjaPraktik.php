abed9fd2d51b9db437143c30413d9c45bd8f0b57
ravihabibillah <ravihabibillah44@gmail.com>
2020-06-19 22:21:57 +0700

    [ravi] penambahan fitur admin (fasilitas,skripsi dan kerja praktik)

---
+++ application/controllers/KerjaPraktik.php
@@ -0,0 +1,124 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class KerjaPraktik extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('MKerjaPraktik', 'kerjapraktiks');
+	}
+
+	public function lists()
+	{
+		$data['kps'] = $this->kerjapraktiks->getAll();
+		$this->load->view('admin/kerja_praktik', $data);
+	}
+
+	public function create()
+	{
+		$this->form_validation->set_rules('Keterangan', 'description', 'min_Length[3]');
+		if (empty($_FILES['file']['tmp_name'])) {
+			$this->form_validation->set_rules('file', 'File', 'required');
+		}
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('admin/addKerjaPraktik');
+		} else {
+			$data = [
+				'title' => $this->input->post('title', true),
+				'description' => $this->input->post('description', true),
+				'status' => $this->input->post('status', true) ? $this->input->post('status', true) :  0
+			];
+
+			if ($_FILES['file']['tmp_name']) {
+				$config['file_name'] = changeFileName($_FILES['file']);
+				$config['allowed_types'] = "jpg|png|jpeg|gif|doc|docx|xls|pdf";
+				$config['max_size'] = 10000;
+				$config['upload_path'] = "./assets/file/kerja_praktik/";
+				$config['remove_spaces'] = false;
+
+				if (!uploadFile($config, 'file')) {
+					$error = $this->upload->display_errors();
+					$this->session->set_flashdata('message', '<div class="alert alert-danger">Gagal mengupload file!<br>' . $error . '</div>');
+					redirect('admin/kerja_praktik');
+				}
+
+				$data['file'] = $config['upload_path'] . $config['file_name'];
+			}
+
+			if ($this->kerjapraktiks->insert($data)) {
+				$this->session->set_flashdata('message', '<div class="alert alert-success">Berhasil menambahkan data</div>');
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger">Gagal menambahkan data</div>');
+			}
+
+			redirect('admin/kerja_praktik');
+		}
+	}
+
+	public function edit($id)
+	{
+		$this->form_validation->set_rules('description', 'keterangan', 'min_Length[3]');
+		$data['kp'] = $this->kerjapraktiks->get($id);
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('admin/editKerjaPraktik', $data);
+		} else {
+			$data = [
+			    'title' => $this->input->post('title', true),
+				'description' => $this->input->post('description', true),
+				'status' => $this->input->post('status', true) ? $this->input->post('status', true) :  0
+			];
+
+			if ($_FILES['file']['tmp_name']) {
+				$config['file_name'] = changeFileName($_FILES['file']);
+				$config['allowed_types'] = "jpg|png|jpeg|gif|doc|docx|xls|pdf";
+				$config['max_size'] = 10000;
+				$config['upload_path'] = "./assets/file/kerja_praktik/";
+				$config['remove_spaces'] = false;
+                
+                $data['file'] = $this->kerjapraktiks->get($id);
+                
+    			
+				if (!deleteFile($data['file']->file)) {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger">Gagal menghapus file sebelumnya!</div>');
+					redirect('admin/kerja_praktik');
+				}
+
+				if (!uploadFile($config, 'file')) {
+					$error = $this->upload->display_errors();
+					$this->session->set_flashdata('message', '<div class="alert alert-danger">Gagal mengupload file!<br>' . $error . '</div>');
+					redirect('admin/kerja_praktik');
+				}
+
+				$data['file'] = $config['upload_path'] . $config['file_name'];
+			}
+
+			if ($this->kerjapraktiks->update($data, $id)) {
+				$this->session->set_flashdata('message', '<div class="alert alert-success">Berhasil memperbaharui data</div>');
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger">Gagal memperbaharui data</div>');
+			}
+
+			redirect('admin/kerja_praktik');
+		}
+	}
+
+	public function delete($id)
+	{
+		$kerja_praktik = $this->kerjapraktiks->get($id);
+		if (!deleteFile($kerja_praktik->file)) {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger">Gagal menghapus file sebelumnya!</div>');
+			redirect('admin/kerja_praktik');
+		}
+
+		if ($this->kerjapraktiks->delete($id)) {
+			$this->session->set_flashdata("message", '<div class="alert alert-success">Berhasil menghapus data Kerja Praktik</div>');
+		} else {
+			$this->session->set_flashdata("message", '<div class="alert alert-danger">Gagal menghapus data Kerja Praktik</div>');
+		}
+
+		redirect('admin/kerja_praktik');
+	}
+}
---
+++ application/views/admin/kerja_praktik.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'head.php'; ?>
+
+<body>
+
+	<div id="wrapper">
+
+		<!-- Navigation -->
+		<?php include 'navbar.php'; ?>
+
+		<div id="page-wrapper">
+			<div class="row">
+				<div class="col-lg-12">
+					<h3 class="page-header">Daftar Informasi Kerja Praktik</h3>
+				</div>
+				<!-- /.col-lg-12 -->
+			</div>
+			<!-- /.row -->
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<div class="mb-3">
+								<a href="<?php echo base_url('admin/kerja_praktik/create') ?>"><button type="button" class="btn btn-primary btn-sm">Tambah Informasi Kerja Praktik</button></a>
+							</div>
+						</div>
+						<div class="panel-body">
+							<?php echo $this->session->flashdata('message'); ?>
+							<div class="dataTable_wrapper">
+								<table class="table table-striped table-bordered table-hover" id="dataTables-example">
+									<thead>
+										<th>No</th>
+										<th>Judul</th>
+										<th>Tanggal</th>
+										<th>Deskripsi</th>
+										<th>Status</th>
+										<th>Aksi</th>
+									</thead>
+									<tbody>
+										<?php $no = 1;
+										foreach ($kps as $kp) : ?>
+											<tr>
+												<td width="10px" class="text-center"><?php echo $no++; ?>
+												<td><?php echo $kp->title; ?></td>
+												<td><?php echo date('H:i d-F-Y', strtotime($kp->created_at)); ?></td>
+												<td><?php echo substr(html_entity_decode($kp->description), 0, 200); ?>...</td>
+												<td><?php echo is_publish($kp->status); ?></td>
+												<td>
+													<a href="<?php echo base_url('admin/kerja_praktik/edit/') . $kp->id; ?>" class="btn btn-xs btn-primary mr-3">Edit</a>
+													<a href="<?php echo base_url('admin/kerja_praktik/delete/') . $kp->id; ?>" class="btn btn-xs btn-danger" onclick="return confirm('apakah kamu yakin ingin menghapusnya?')">Hapus</a>
+												</td>
+											</tr>
+										<?php endforeach; ?>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+					<!-- /.panel -->
+				</div>
+				<!-- /.col-lg-12 -->
+			</div>
+		</div>
+	</div>
+	<!-- /#wrapper -->
+
+	<?php include 'foot.php'; ?>
+	<script>
+		tinymce.init({
+			plugins: "paste",
+			paste_as_text: true
+		});
+	</script>
+</body>
+
+</html>
